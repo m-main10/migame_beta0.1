3ff0e322f8db59ca38c88ed224e4d454509ad4aa
m-main10 <mihailburcinsij@gmail.com>
2026-05-11 11:43:33 +0700

    Create color.py

---
+++ color.py
@@ -0,0 +1,9 @@
+import pygame
+class Color:
+    def __init__(self):
+        self.tupe="color"
+    def _rgb(self,r,g,b):
+        self.r=r
+        self.b =b
+        self.g=g
+        return (r,g,b)
